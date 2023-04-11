668fe2b6281d0805c0144218303f60709ab5469d
eexy <eexdhe@gmail.com>
2023-04-11 19:27:48 +0200

    feat: create responsive menu

---
+++ src/scripts/menu-script.ts
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.burger-btn');
+const layer = document.querySelector('');
+
+menuBtn?.addEventListener('click', () => {
+  console.info('lol');
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
-    colors: {
-      background: '#F5DFCB',
-      main: '#172554',
-      white: '#ffffff',
+    extend: {
+      colors: {
+        background: '#F5DFCB',
+        main: '#172554',
+      },
     },
   },
   plugins: [],
